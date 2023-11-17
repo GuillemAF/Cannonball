1e3f9fbe3081a1bf6e764c2167fe1c36a0a5bb4c
GuillemAF <guimagullo@gmail.com>
2023-11-17 03:14:59 -0500

    He afegit la opció de escollir si vols que s'extreguin els headers o no

--- Cannonball.sh
+++ Cannonball.sh
@@ -27,49 +27,78 @@ bash ../testssl.sh/testssl.sh --logfile TESTSSL_out $DOMAIN
 
 #Collecting headers with theHarvester links:
 
-for i in $(cat TH_out.json | jq -r '.hosts[]')
-do
-        echo "Domain: $i" >> headers
-        echo -e "\n" >> headers
-	echo "port: 80" >> headers
-        curl -I $i >> headers
-	echo "port: 443" >> headers
-	curl -I $i:443 >> headers
-	echo "port: 8080" >> headers
-        curl -I $i:8080 >> headers
-	echo "port: 8081" >> headers
-        curl -I $i:8081 >> headers
-	echo "port: 3080" >> headers
-        curl -I $i:3080 >> headers
-	echo "port: 8443" >> headers
-        curl -I $i:8443 >> headers
-	echo "port: 4443" >> headers
-        curl -I $i:4443 >> headers
-	echo "port: 6443" >> headers
-        curl -I $i:6443 >> headers
+echo "All the tools worked correctly. Do you want to extract http headers?"
+
+read -p "Y/n: " HEADERS
+
+if [ $HEADERS == "Y" ] || [ $HEADERS == "y" ]
+then
+        echo "Domain: $DOMAIN"
+        echo "port: 80" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN >> headers
+        echo "port: 443" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:443 >> headers
+        echo "port: 8080" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:8080 >> headers
+        echo "port: 8081" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:8081 >> headers
+        echo "port: 3080" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:3080 >> headers
+        echo "port: 8443" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:8443 >> headers
+        echo "port: 4443" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:4443 >> headers
+        echo "port: 6443" >> headers
+        curl -m 5 --connect-timeout 5 -I $DOMAIN:6443 >> headers
         echo -e "\n\n"
-done
+
+        for i in $(cat TH_out.json | jq -r '.hosts[]')
+        do
+                echo "Domain: $i" >> headers
+                echo -e "\n" >> headers
+                echo "port: 80" >> headers
+                curl -m 5 --connect-timeout 5 -I $i >> headers
+                echo "port: 443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:443 >> headers
+                echo "port: 8080" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8080 >> headers
+                echo "port: 8081" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8081 >> headers
+                echo "port: 3080" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:3080 >> headers
+                echo "port: 8443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8443 >> headers
+                echo "port: 4443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:4443 >> headers
+                echo "port: 6443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:6443 >> headers
+                echo -e "\n\n"
+        done
+
+        for i in $(cat TH_out.json | jq -r '.ips[]')
+        do
+                echo "IP: $i" >> headers
+                echo -e "\n" >> headers
+                echo "port: 80" >> headers
+                curl -m 5 --connect-timeout 5 -I $i >> headers
+                echo "port: 443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:443 >> headers
+                echo "port: 8080" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8080 >> headers
+                echo "port: 8081" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8081 >> headers
+                echo "port: 3080" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:3080 >> headers
+                echo "port: 8443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:8443 >> headers
+                echo "port: 4443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:4443 >> headers
+                echo "port: 6443" >> headers
+                curl -m 5 --connect-timeout 5 -I $i:6443 >> headers
+                echo -e "\n\n"
+        done
+
+fi
 
 
-for i in $(cat TH_out.json | jq -r '.ips[]')
-do
-        echo "IP: $i" >> headers
-        echo -e "\n" >> headers
-	echo "port: 80" >> headers
-        curl -I $i >> headers
-	echo "port: 443" >> headers
-	curl -I $i:443 >> headers
-	echo "port: 8080" >> headers
-        curl -I $i:8080 >> headers
-	echo "port: 8081" >> headers
-        curl -I $i:8081 >> headers
-	echo "port: 3080" >> headers
-        curl -I $i:3080 >> headers
-	echo "port: 8443" >> headers
-        curl -I $i:8443 >> headers
-	echo "port: 4443" >> headers
-        curl -I $i:4443 >> headers
-	echo "port: 6443" >> headers
-        curl -I $i:6443 >> headers
-        echo -e "\n\n"
-done
+
--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # Cannonball
 Cannonball is a pentest tool used for information gathering and OSINT. It's centered in enterprise security where you'll have to document and download what you find in a very structured way. This tool is a cluster of many local and online OSINT softwares created by other users. What I've made it's just put all of those together.  
 
+## Installation guide
+To install this package you will need:
+ - A linux machine (Kali is recomended because has many of the tools this script use already installed)
+ - Github installed
+ - TheHarvester installed
+ - Amass installed  
+
+To proceed with the installation you'll have to run this command on your bash shell:
+
+```bash
+git clone https://github.com/GuillemAF/Cannonball.git
+```
+also you will have to have TheHarvester and Amass isntalled too. You can do this by typing:
+
+```bash
+apt-get install theharvester & apt-get install amass
+```
+
+That's it. You have succesfully installed Cannonball on your machine.
+
 ## How does it work?
 When you have completed the process of installation, you will have to execute: 
 ```bash
 bash Cannonball.sh
 ```
-Then a text appears. You'll have to introduce your domain and your directory name and wait. When the process is finished, you will have a folder with the name that you puted. Inside you'll find all the output files ready to be documented. 
+Then a text appears. You'll have to introduce your domain and your directory name and wait. When the process is finished, you will have a folder with the name that you introduced previousley. Inside you'll find all the output files ready to be documented.  
+This way you can have as many directories as you want containing the domains that you want. 
 
 
 ### What modules does Cannonball use?
